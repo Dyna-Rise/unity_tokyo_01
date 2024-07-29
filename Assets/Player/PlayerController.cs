f1599f41079c28dfd89a7b869711f5f19ac2780e
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-29 12:16:57 +0900

    0729 12:16

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody; //Rigidbody2D型の変数
     float axisH = 0.0f; //入力の力
+    public float speed = 3.0f; //プレイヤーの移動速度
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,18 @@ public class PlayerController : MonoBehaviour
     {
         //水平方向にまつわるキー（左右キー・ AとD）がおされている場合、左方向なら-1、右方向なら1、何も押されていなければ0の値を返すメソッド→GetAixsRaw
         axisH = Input.GetAxisRaw("Horizontal");
-    }
+        if(axisH > 0.0f) //axisHがプラスの値の時＝右が押された時
+        {
+            transform.localScale = new Vector2(1,1); //イラストの向きを右向き
+        }
+        else if(axisH < 0.0f) //axisHがマイナスの値の時＝左が押された時
+        {
+            transform.localScale = new Vector2(-1, 1); //イラストの向きを左向き
+        }
 
-    void FixedUpdate()
+        void FixedUpdate()
     {
         //rbody.velocity.y　→　y方向の力に関しては成り行き
-        rbody.velocity = new Vector2(axisH * 3.0f,rbody.velocity.y);
+        rbody.velocity = new Vector2(axisH * speed,rbody.velocity.y);
     }
 }
