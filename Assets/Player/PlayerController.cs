1a2e64c28c1562adf99e9a28ad36533a8d742fc1
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-29 12:21:03 +0900

    0729 12:20

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -20,18 +20,20 @@ public class PlayerController : MonoBehaviour
     {
         //水平方向にまつわるキー（左右キー・ AとD）がおされている場合、左方向なら-1、右方向なら1、何も押されていなければ0の値を返すメソッド→GetAixsRaw
         axisH = Input.GetAxisRaw("Horizontal");
-        if(axisH > 0.0f) //axisHがプラスの値の時＝右が押された時
+        if (axisH > 0.0f) //axisHがプラスの値の時＝右が押された時
         {
-            transform.localScale = new Vector2(1,1); //イラストの向きを右向き
+            transform.localScale = new Vector2(1, 1); //イラストの向きを右向き
         }
-        else if(axisH < 0.0f) //axisHがマイナスの値の時＝左が押された時
+        else if (axisH < 0.0f) //axisHがマイナスの値の時＝左が押された時
         {
             transform.localScale = new Vector2(-1, 1); //イラストの向きを左向き
         }
+    }
 
-        void FixedUpdate()
+    void FixedUpdate()
     {
         //rbody.velocity.y　→　y方向の力に関しては成り行き
-        rbody.velocity = new Vector2(axisH * speed,rbody.velocity.y);
+        rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
     }
+
 }
