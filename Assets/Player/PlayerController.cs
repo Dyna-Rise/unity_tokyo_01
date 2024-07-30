00b67443891f80d2aaecbff1e4ea962902d3198a
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-30 10:39:04 +0900

    0730 10:39

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rbody; //Rigidbody2D型の変数
     float axisH = 0.0f; //入力の力
     public float speed = 3.0f; //プレイヤーの移動速度
+
+    public float jump = 9.0f; //ジャンプ力
+    public LayerMask groundLayer; //判定対象のレイヤー
+    bool goJump = false; //ジャンプ開始フラグ
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,8 +40,26 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //rbody.velocity.y　→　y方向の力に関しては成り行き
-        rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        bool onGround = Physics2D.CircleCast(transform.position,
+            0.2f,
+            Vector2.down,
+            0.0f,
+            groundLayer);
+        if (onGround || axisH != 0)
+        {
+            //rbody.velocity.y　→　y方向の力に関しては成り行き
+            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        }
+        if (onGround && goJump)
+        {
+            Vector2 jumpPw = new Vector2(0, jump);
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+            goJump = false;
+        }
     }
 
+    public void Jump()
+    {
+        goJump = true;
+    }
 }
