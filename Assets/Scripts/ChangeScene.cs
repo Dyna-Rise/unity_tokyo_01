992cf2c03c3080af77d17b064b1fd30923d87dd2
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-07-31 16:04:41 +0900

    0731 16:04

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //切り替え命令が用意されている名前空間
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName; //目的の（切り替え先の)シーン名が入る
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //シーン切替の自作メソッド
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);　//LoadSceneメソッドで目的のシーン名を指定
+    }
+}
