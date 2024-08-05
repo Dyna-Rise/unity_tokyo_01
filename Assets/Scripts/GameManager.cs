e6a76ff2e003625404e7dbe4858656015caa8a32
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-08-05 15:04:18 +0900

    0805 15:04

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,12 +20,17 @@ public class GameManager : MonoBehaviour
     public GameObject timeText; //時間テキスト
     TimeController timeCnt; //TimeControllerクラス
 
+
+    public GameObject scoreText; //スコアのUI
+    public static int totalScore; //全編通して引き継いでいくスコア
+    public int stageScore = 0; //そのステージの合計得点※次のステージに移る頃には消滅
+
     // Start is called before the first frame update
     void Start()
     {
         //時間差で「ゲームスタートを非表示にするメソッド」を発動
         //第1引数→メソッド名
-        Invoke("InactiveImage",1.0f);
+        Invoke("InactiveImage", 1.0f);
 
         //パネルは即非表示
         panel.SetActive(false);
@@ -34,21 +39,24 @@ public class GameManager : MonoBehaviour
         //++時間制限追加+++
         //TimeControllerを取得
         timeCnt = GetComponent<TimeController>();
-        if(timeCnt != null)
+        if (timeCnt != null)
         {
             //カウントダウン前提で、カウントダウンする気がない時
-            if(timeCnt.gameTime == 0.0f)
+            if (timeCnt.gameTime == 0.0f)
             {
                 timeBar.SetActive(false); //時間制限なしなら隠す
             }
 
         }
+
+        //自作メソッド ※まずはUIにその面がスタートした時のスコアを表示
+        UpdateScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(PlayerController.gameState == "gameclear")
+        if (PlayerController.gameState == "gameclear")
         {
             mainImage.SetActive(true); //非表示にしていた画像を表示に戻す
             panel.SetActive(true);//非表示にしていたパネルを表示に戻す
@@ -65,13 +73,24 @@ public class GameManager : MonoBehaviour
 
 
             //++時間制限追加+++
-            if(timeCnt != null)
+            if (timeCnt != null)
             {
                 timeCnt.isTimeOver = true; //カウント停止
+
+                //スコアも確定
+                //残時間を整数化して変数timeに保管
+                int time = (int)timeCnt.displayTime;
+                totalScore += time * 10; //残時間x10をタイムボーナスとして加算
             }
+
+            //stageScoreもトータルに加算
+            totalScore += stageScore;
+            stageScore = 0; //二重にならないようにstageScoreはリセット
+            UpdateScore(); //UIに反映
+
         }
 
-        else if(PlayerController.gameState == "gameover")
+        else if (PlayerController.gameState == "gameover")
         {
             mainImage.SetActive(true); //非表示にしていた画像を表示に戻す
             panel.SetActive(true);//非表示にしていたパネルを表示に戻す
@@ -94,29 +113,38 @@ public class GameManager : MonoBehaviour
             }
 
         }
-        else if(PlayerController.gameState == "playing")
+        else if (PlayerController.gameState == "playing")
         {
             //ゲーム中のUI
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             //PlayerControllerを取得
             PlayerController playerCnt = player.GetComponent<PlayerController>();
 
-            if(timeCnt != null)
+            if (timeCnt != null)
             {
-                if(timeCnt.gameTime > 0.0f)
+                if (timeCnt.gameTime > 0.0f)
                 {
                     //整数に代入することで小数を切り捨てる
                     int time = (int)timeCnt.displayTime;
                     //タイム更新
                     timeText.GetComponent<TextMeshProUGUI>().text = time.ToString();
-                    
+
                     //タイムオーバー
-                    if(time == 0)
+                    if (time == 0)
                     {
                         playerCnt.GameOver(); //ゲームオーバーにする
                     }
                 }
             }
+
+            //スコアの追加
+            if(playerCnt.score != 0) //プレイヤーがアイテムを取った時
+            {
+                stageScore += playerCnt.score;
+                playerCnt.score = 0;
+                UpdateScore(); //表に現スコアを表示
+            }
+
         }
     }
 
@@ -124,5 +152,13 @@ public class GameManager : MonoBehaviour
     void InactiveImage()
     {
         mainImage.SetActive(false);
+    }
+
+
+    void UpdateScore()
+    {
+        //そのステージで入手したスコア+前ステージまでの確定スコアを足す
+        int score = stageScore + totalScore;
+        scoreText.GetComponent<TextMeshProUGUI>().text = score.ToString();
     }
 }
