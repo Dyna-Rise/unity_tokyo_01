517e39bae247152ec6eea244b7683fff1a13a11f
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-08-06 14:03:43 +0900

    0806 MovingBlockのコメント更新とギズモ修正 14:03

--- Assets/Scripts/MovingBlock.cs
+++ Assets/Scripts/MovingBlock.cs
@@ -6,11 +6,12 @@ public class MovingBlock : MonoBehaviour
 {
     public float moveX = 0.0f;          //X移動距離
     public float moveY = 0.0f;          //Y移動距離
-    public float times = 0.0f;          //時間
+    public float times = 0.0f;          //何秒で目的地に移動するか
     public float wait = 0.0f;         //停止時間
-    public bool isMoveWhenOn = false;   //乗った時に動くフラグ
+    public bool isMoveWhenOn = false;   //乗った時にはじめて動くようにするかどうかフラグ
 
-    public bool isCanMove = true;       //動くフラグ
+    public bool isCanMove = true;   //動いている最中はtrue
+
     Vector3 startPos; //初期位置
     Vector3 endPos; //移動位置
     bool isReverse = false; //反転フラグ
@@ -20,11 +21,11 @@ public class MovingBlock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //初期位置
-        startPos = transform.position;
-        endPos = new Vector2(startPos.x + moveX, startPos.y + moveY); //移動位置
 
-        if (isMoveWhenOn)
+        startPos = transform.position; //初期位置の取得
+        endPos = new Vector2(startPos.x + moveX, startPos.y + moveY); //目的の位置の取得
+
+        if (isMoveWhenOn) //乗った時にはじめて動くフラグがtrueなら動きを止めとく
         {
             //乗った時に動くので最初は動かさない
             isCanMove = false;
@@ -36,18 +37,24 @@ public class MovingBlock : MonoBehaviour
     {
 
     }
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
-        if (isCanMove)
+        if (isCanMove)//動くフラグがたってないと何もしない
         {
             //移動中
+
             float distance = Vector2.Distance(startPos, endPos); //移動距離
-            float ds = distance / times; //1秒あたりの移動距離 ※timesはエディタで任意設定
-            float df = ds * Time.deltaTime;
-            //1フレームごとの移動距離
-            movep += (df / distance); //Lerpメソッドに指定する移動補完値
+            float ds = distance / times; //1秒あたりどれくらい移動しなければいけないのか ※timesはエディタで任意設定
+            float df = ds * Time.deltaTime;  //1フレームごとの移動距離
+
+            //1フレームあたりの移動距離dfを全体の距離distanceで割る
+            //その1フレームが全体の何%の距離分なのかを計算
+            movep += (df / distance); //Lerpメソッドに指定する移動補完値(進捗率)を計算
 
                 
+            //Lerpメソッドを使ってブロックを動かす
+            //Lerpメソッド ※始点、終点を指定、そのうち今どの進捗率の位置にいるべきかを指定
             if (isReverse)
             {
                 //逆方向移動中...
@@ -58,30 +65,32 @@ public class MovingBlock : MonoBehaviour
             {
                 //正方向移動中...
                 transform.position = Vector2.Lerp(startPos, endPos, movep);
-
             }
 
+            //進捗率が100％に届いた時
             if (movep >= 1.0f)
             {
                 movep = 0.0f; //移動補完値リセット
                 isReverse = !isReverse; //移動を逆転
-                isCanMove = false; //移動停止
-                if(isMoveWhenOn == false)
+                isCanMove = false; //移動を一旦停止
+                if(isMoveWhenOn == false) //プレイヤーが乗って動くタイプじゃない時
                 {
-                    //乗った時に動くフラグOFF
-                    Invoke("Move", wait);
+                    //自動で逆に動きださなければいけない
+                    Invoke("Move", wait); //時間差で動き出すためのMoveメソッドの発動
+
+                    //Invoke→第2引数の秒数待ってから、第1引数のメソッド発動
                 }
             }
         }
     }
 
-    //移動フラグを立てる
+    //移動フラグを立てる　自作メソッド
     public void Move()
     {
         isCanMove = true;
     }
 
-    //移動フラグを下ろす
+    //移動フラグを下ろす 自作メソッド　※スイッチプログラムの時使う
     public void Stop()
     {
         isCanMove = false;
